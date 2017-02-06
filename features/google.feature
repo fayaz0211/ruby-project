9d2b4fd0af0150d8d88babc382e6c5c34d920fb3
fayaz0211 <fayaz0211@gmail.com>
2017-02-06 22:16:57 +0000

    refactored and changed to webdriver from capybara

--- features/google.feature
+++ features/google.feature
@@ -2,5 +2,5 @@ Feature: Search an item on google
 
   Scenario:
     Given I am on google page
-    When I search for word "ruby"
+    When I search for word ruby
     Then I should see the result
--- features/step_definitions/google_steps.rb
+++ features/step_definitions/google_steps.rb
@@ -1,25 +1,32 @@
 require_relative '../../features/support/env'
 require 'capybara/cucumber'
+require 'selenium-webdriver'
 
-Capybara.default_driver = :selenium
-
+# Capybara.default_driver = :selenium
+driver = Selenium::WebDriver.for :firefox
 Given(/^I am on google page$/) do
-  visit 'http://www.google.co.uk'
+  # visit 'http://www.google.co.uk'
+  driver.get 'http://www.google.co.uk'
+
 end
 
-When(/^I search for word "([^"]*)"$/) do |arg1|
+When(/^I search for word (.*)$/) do |arg1|
 
-   fill_in('lst-ib', :with => arg1)
-  # click_button('btnK')
-  #   find(:xpath, "//*[@id='tsf']//center/input[1]").click()
-   Capybara.add_selector(:name) do
-     xpath { |name| XPath.descendant[XPath.attr(:name) == name.to_s] }
-   end
+#    fill_in('lst-ib', :with => arg1)
+#   # click_button('btnK')
+#   #   find(:xpath, "//*[@id='tsf']//center/input[1]").click()
+#    Capybara.add_selector(:name) do
+#      xpath { |name| XPath.descendant[XPath.attr(:name) == name.to_s] }
+#    end
+#
+# find(:name , 'btnG').click()
+#   puts 'test passed'
 
-find(:name , 'btnG').click()
-  puts 'test passed'
+  driver.find_element(:name,'q').send_keys(arg1)
    end
 
 Then(/^I should see the result$/) do
-
+driver.find_element(:name,'btnG').click
+  puts 'test passed!!!...'
+  driver.quit
 end
